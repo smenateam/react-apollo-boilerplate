be72210fe2f3c5fbfb3e58284dfa0e3530f7b139
sovofeel <sovofeel@gmail.com>
2021-04-02 15:24:20 +0500

    fix(deps): fix broken dependensies

--- src/apollo/hooks.ts
+++ src/apollo/hooks.ts
@@ -1,61 +1,76 @@
 import * as Apollo from '@apollo/client'
 import { ApolloError } from '@apollo/client'
-import { sentryCaptureError } from 'src/sentry/captureErrors'
+import { redirectHandler } from 'src/services/redirectWhenError'
+import { sentryCaptureError } from 'src/vendor/sentry/captureErrors'
 import {
   QueryHookOptions,
   MutationHookOptions,
   LazyQueryHookOptions,
   Variables,
 } from './hooks.types'
+import { getErrorFromResponse } from './utils'
 
 export * from '@apollo/client'
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export function useQuery<TData = any, TVariables = Apollo.OperationVariables>(
+export function useQuery<TData = unknown, TVariables = Apollo.OperationVariables>(
   query: Apollo.DocumentNode,
   options?: QueryHookOptions<TData, TVariables>
 ): Apollo.QueryResult<TData, TVariables> {
   return Apollo.useQuery<TData, TVariables>(query, {
     ...options,
-    onCompleted: (data: TData) => {
+    onCompleted: data => {
+      const error = getErrorFromResponse(data)
+
+      if (error?.__typename) {
+        redirectHandler.createPath({ name: error.__typename, meta: error.message })
+      }
+
       if (options?.onCompleted) options.onCompleted(data)
     },
     onError: (error: ApolloError) => {
       const variables = options?.variables as Variables
       sentryCaptureError({ error, variables, operation: query })
-
       if (options?.onError) options.onError(error)
     },
   })
 }
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export function useLazyQuery<TData = any, TVariables = Apollo.OperationVariables>(
+export function useLazyQuery<TData = unknown, TVariables = Apollo.OperationVariables>(
   query: Apollo.DocumentNode,
   options?: LazyQueryHookOptions<TData, TVariables>
 ): Apollo.QueryTuple<TData, TVariables> {
   return Apollo.useLazyQuery<TData, TVariables>(query, {
     ...options,
-    onCompleted: (data: TData) => {
+    onCompleted: data => {
+      const error = getErrorFromResponse(data)
+
+      if (error?.__typename) {
+        redirectHandler.createPath({ name: error.__typename, meta: error.message })
+      }
+
       if (options?.onCompleted) options.onCompleted(data)
     },
     onError: (error: ApolloError) => {
       const variables = options?.variables as Variables
       sentryCaptureError({ error, variables, operation: query })
-
       if (options?.onError) options.onError(error)
     },
   })
 }
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export function useMutation<TData = any, TVariables = Apollo.OperationVariables>(
+export function useMutation<TData = unknown, TVariables = Apollo.OperationVariables>(
   mutation: Apollo.DocumentNode,
   options?: MutationHookOptions<TData, TVariables>
 ): Apollo.MutationTuple<TData, TVariables> {
   return Apollo.useMutation<TData, TVariables>(mutation, {
     ...options,
-    onCompleted: (data: TData) => {
+    onCompleted: data => {
+      const error = getErrorFromResponse(data)
+
+      if (error?.__typename) {
+        redirectHandler.createPath({ name: error.__typename, meta: error.message })
+      }
+
       if (options?.onCompleted) options.onCompleted(data)
     },
     onError: (error: ApolloError) => {
