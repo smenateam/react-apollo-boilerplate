71aa6d7b0dcdd979e5ee437a653b22ead584ee34
sovofeel <sovofeel@gmail.com>
2021-07-08 14:41:08 +0500

    bump husky

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,8 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
 import Main from 'src/components/Main'
 
-const App = (): ReactElement => (
+const App = (): ReactElement => {
+  (
   <Router>
     <Route>
       <Switch>
